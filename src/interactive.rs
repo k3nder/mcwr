05ac926c44ef5211be256e6d65f8697c1cb260bf
k3nder <k3nder@outlook.es>
2025-03-25 17:03:09 +0100

    implement welcome message and prompt user action

--- src/interactive.rs
+++ src/interactive.rs
@@ -1,5 +1,45 @@
+use console::{style, Style};
+use dialoguer::{theme::ColorfulTheme, Select};
+
+use crate::mvers;
+
 pub fn run() {
-    let versions = mvers::get_versions().map(|version| {
-        println!("Version: {}", versions);
-    });
+    println!("Running interactive mode...");
+    welcome();
+    prompt_user_action();
+    todo!("Implement the rest of the interactive mode");
+}
+fn prompt_user_action() {
+    open_select(
+        style("What do you want to do?")
+            .bold()
+            .bright()
+            .color256(208)
+            .to_string()
+            .as_str(),
+        vec![
+            "Show versions",
+            "Show downloaded versions",
+            "Download a version",
+            "Delete a version",
+            "Run game",
+            style("Exit").bold().bright().red().to_string().as_str(),
+        ],
+    );
+}
+fn welcome() {
+    println!(
+        "{} {} {}",
+        style("Welcome to the").bold(),
+        style("interactive").bold().green(),
+        style("mode").bold(),
+    );
+}
+fn open_select(prompt: &str, options: Vec<&str>) -> String {
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt(prompt)
+        .items(&options)
+        .interact()
+        .unwrap();
+    options[selection].to_string().clone()
 }
