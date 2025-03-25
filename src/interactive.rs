1e979978c0986570b5e6c5bf3db746fc6b630d41
k3nder <k3nder@outlook.es>
2025-03-25 17:31:15 +0100

    Implement list downloaded versions

--- src/interactive.rs
+++ src/interactive.rs
@@ -1,31 +1,101 @@
-use console::{style, Style};
+use console::{style, Style, Term};
 use dialoguer::{theme::ColorfulTheme, Select};
 
 use crate::mvers;
 
 pub fn run() {
+    let term = Term::stdout();
+    term.clear_screen().unwrap();
+
     println!("Running interactive mode...");
     welcome();
-    prompt_user_action();
-    todo!("Implement the rest of the interactive mode");
+    loop {
+        match prompt_user_action() {
+            Action::ShowDownloadedVersions => {
+                show_downloaded_versions(&term);
+            }
+            Action::DownloadVersion => {
+                download_version(&term);
+            }
+            Action::Configurations => {
+                configurations(&term);
+            }
+            Action::DeleteVersion => {
+                delete_version(&term);
+            }
+            Action::RunGame => {
+                run_game(&term);
+            }
+            Action::Exit => {
+                println!("Exiting interactive mode...");
+                break;
+            }
+        }
+    }
 }
-fn prompt_user_action() {
-    open_select(
-        style("What do you want to do?")
-            .bold()
-            .bright()
-            .color256(208)
-            .to_string()
-            .as_str(),
+
+fn configurations(term: &Term) -> _ {
+    todo!()
+}
+fn show_downloaded_versions(term: &Term) {
+    print_system_message("Ok... Here are all the downloaded versions");
+    let versions = mvers::list();
+    if versions.is_empty() {
+        term.move_cursor_up(1).unwrap();
+        term.clear_line().unwrap();
+        print_system_message("Oh... you don't have downloaded versions");
+    } else {
+        versions.iter().for_each(|(name, _)| {
+            println!("{}", name);
+        });
+    }
+}
+fn download_version(term: &Term) {
+    print_system_message("you want to start a new adventure, let's start");
+}
+fn select_version(term: &Term) {
+    let versions = mvers::list();
+    let selection = dialoguer::FuzzySelect::with_theme(&ColorfulTheme::default())
+        .with_prompt(system_message("Select a version"))
+        .default(0)
+        .interact()
+        .unwrap();
+}
+fn delete_version(term: &Term) {
+    println!("Deleting version...");
+}
+fn run_game(term: &Term) {
+    println!("Running game...");
+}
+
+enum Action {
+    ShowDownloadedVersions,
+    DownloadVersion,
+    Configurations,
+    DeleteVersion,
+    RunGame,
+    Exit,
+}
+fn prompt_user_action() -> Action {
+    let selection = open_select(
+        system_message("What do you want to do?").as_str(),
         vec![
-            "Show versions",
             "Show downloaded versions",
             "Download a version",
+            "Configurations",
             "Delete a version",
             "Run game",
             style("Exit").bold().bright().red().to_string().as_str(),
         ],
     );
+    match selection {
+        0 => Action::ShowDownloadedVersions,
+        1 => Action::DownloadVersion,
+        2 => Action::Configurations,
+        3 => Action::DeleteVersion,
+        4 => Action::RunGame,
+        _ => Action::Exit,
+    }
 }
 fn welcome() {
     println!(
@@ -35,11 +105,17 @@ fn welcome() {
         style("mode").bold(),
     );
 }
-fn open_select(prompt: &str, options: Vec<&str>) -> String {
+fn open_select(prompt: &str, options: Vec<&str>) -> usize {
     let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt(prompt)
         .items(&options)
         .interact()
         .unwrap();
-    options[selection].to_string().clone()
+    selection
+}
+fn system_message(message: &str) -> String {
+    format!("{}", style(message).bold().bright().color256(208))
+}
+fn print_system_message(message: &str) {
+    println!("{}", system_message(message));
 }
